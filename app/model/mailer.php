2c4235b2a83a025191a596ed1e252c95c3697674
greg <szabogergely07@gmail.com>
2018-08-18 21:54:47 +0200

    Add sendgrid instead of phpmailer

--- app/model/mailer.php
+++ app/model/mailer.php
@@ -7,33 +7,54 @@ use phpmailer\Exception;
 
 class mailer {
 
-	public function __construct($email, $name, $subject, $body) {
-		// Send registration confirmation email
-		$mail = new PHPMailer();    
-		// Passing `true` enables exceptions
+	public function __construct($mail, $name, $subject, $body) {
 
-	    //Server settings
-	    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-	    $mail->isSMTP(true);                                      // Set mailer to use SMTP
-	    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-	    $mail->Username = 'szabogergely07@gmail.com';                 // SMTP username
-	    $mail->Password = GMAIL_PASS;                           // SMTP password
-	    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-	    $mail->Port = 465;                                    // TCP port to connect to
+		$email = new \SendGrid\Mail\Mail(); 
+		$email->setFrom("szabogergely07@gmail.com", "SportBuddy");
+		$email->setSubject($subject);
+		$email->addTo("szabogergely07@gmail.com", "Greg Taylor");
+		$email->addContent("text/plain", $body);
+		// $email->addContent(
+		//     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+		
+		$sendgrid = new \SendGrid(SENDGRID_API);
+		try {
+		    $response = $sendgrid->send($email);
+		    // print $response->statusCode() . "\n";
+		    // print_r($response->headers());
+		    // print $response->body() . "\n";
+		} catch (Exception $e) {
+		    echo 'Caught exception: '. $e->getMessage() ."\n";
+		}
 
-	    $mail->setFrom('sportbuddy@sportbuddy.com', 'SportBuddy');
-	    $mail->addAddress($email, $name);
-	    $mail->addBCC('szabogergely07@gmail.com', 'Greg');          // Add a recipient
-	    $mail->addReplyTo('szabogergely07@gmail.com', 'Information');
+
+
+		// // Send registration confirmation email
+		// $mail = new PHPMailer();    
+		// // Passing `true` enables exceptions
+
+	 //    //Server settings
+	 //    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+	 //    $mail->isSMTP(true);                                      // Set mailer to use SMTP
+	 //    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	 //    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+	 //    $mail->Username = 'szabogergely07@gmail.com';                 // SMTP username
+	 //    $mail->Password = GMAIL_PASS;                           // SMTP password
+	 //    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+	 //    $mail->Port = 465;                                    // TCP port to connect to
+
+	 //    $mail->setFrom('sportbuddy@sportbuddy.com', 'SportBuddy');
+	 //    $mail->addAddress($email, $name);
+	 //    $mail->addBCC('szabogergely07@gmail.com', 'Greg');          // Add a recipient
+	 //    $mail->addReplyTo('szabogergely07@gmail.com', 'Information');
 	    
-	    //Content
-	    $mail->isHTML(true);                                  // Set email format to HTML
-	    $mail->Subject = $subject;
-	    $mail->Body    = $body;
-	    $mail->AltBody = $body;
+	 //    //Content
+	 //    $mail->isHTML(true);                                  // Set email format to HTML
+	 //    $mail->Subject = $subject;
+	 //    $mail->Body    = $body;
+	 //    $mail->AltBody = $body;
 
-	    $mail->send();
+	 //    $mail->send();
 	}
 
 
